78e21b512714933eaf84cfdb94fa3fd72fcfc06b
jkzares <164260157+jkzares@users.noreply.github.com>
2024-03-21 04:11:51 +0000

    nextjs initialized

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      The Home page
+    </main>
+  );
+}
---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const navigation = [
+    {
+        id: "home",
+        url: '/',
+        title: "Home"
+    },
+    {
+        id: "blog",
+        url: '/blog',
+        title: "Blogs"
+    },
+    {
+        id: "about",
+        url: '/about',
+        title: "About"
+    },
+]
